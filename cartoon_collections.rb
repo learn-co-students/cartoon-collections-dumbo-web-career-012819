95743fdd2d2bad0272c3a38b51fa09a37953ca34
Kate F <follett.katie@gmail.com>
2019-01-22 19:28:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls (planeteer_calls)
-  planeteer_calls,any? do |call|
+  planeteer_calls.any? do |call|
     call.length > 4
   end
 end
